90d7bfb97b43c021a162ec08f31acb36d9429fbd
YOUR_NAME <YOUR_EMAIL>
2023-10-14 12:22:10 +0300

    14.10

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     app_title: str = 'Сервис пожертвований'
     app_description: str = 'Пожертвования котикам'
-    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+    database_url: str = 'sqlite+aiosqlite:///./qrkot.db'
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
